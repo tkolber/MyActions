b388b5ad82741a3b942a75944aa87856c9126704
Tzahi Kolber <43411551+tkolber@users.noreply.github.com>
2023-02-27 19:07:49 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,2 +1,4 @@
-Write-Host -ForegroundColor Green "start simple"
-Write-Host -ForegroundColor Magenta "Done running the script"
+Write-Host -ForegroundColor Green "Start running GitHubActions test"
+$I = 0
+1..100 | % {write-host "Github Actions" -ForegroundColor green ; $i=$i+1 ; Write-Host $i} 
+Write-Host -ForegroundColor Magenta "Done running GitHubActions test"
